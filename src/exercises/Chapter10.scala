57bf98e758668dd62a008fb9a69d202190302954
Alexander Smaga <smagaan@gmail.com>
2015-12-09 23:29:08 +0200

    Update Chapter10.scala

--- src/exercises/Chapter10.scala
+++ src/exercises/Chapter10.scala
@@ -1,13 +1,15 @@
-object Chapter10 extends App {
-  {
-    import java.awt.geom.Ellipse2D.{Double => Ellips}
 
-    var test = new Ellips(2, 3, 4, 5) with RectangleLike
-    println(test)
-    test.grow(2, 3)
-    println(test)
-    test.translate(2, 3)
-    println(test)
+object Chapter10 extends App {
+  // 1
+  {
+    import java.awt.geom.Ellipse2D.{Double => Ellipse}
+
+    val ellipse = new Ellipse(2, 3, 4, 5) with RectangleLike
+    println(ellipse)
+    ellipse.grow(2, 3)
+    println(ellipse)
+    ellipse.translate(2, 3)
+    println(ellipse)
 
     trait RectangleLike {
       def translate(x: Double, y: Double) = setFrame(x + getX, y + getY, getWidth, getHeight)
@@ -20,8 +22,29 @@ object Chapter10 extends App {
       def getHeight: Double
 
       override def toString: String = {
-        "X: " + getX.toString + " Y:" + getY.toString + " W: " + getWidth.toString + " H:" + getHeight.toString
+        "X: %.2f Y: %.2f W: %.2f H: %.2f" format(getX, getY, getWidth, getHeight)
       }
+    }
+
+    // 2
+    {
+      import java.awt.Point
+
+      class OrderedPoint(x: Int, y: Int) extends Point(x, y) with Ordered[Point] {
+        def compare(that: Point): Int = {
+          if (getX == that.getX && getY == that.getY) 0
+          else if (getX > that.getX || getX == that.getX && getY > that.getY) 1
+          else -1
+        }
+      }
+
+      val a = new OrderedPoint(1, 5)
+      val b = new OrderedPoint(2, 5)
+      val c = new OrderedPoint(2, 6)
+      val d = new OrderedPoint(2, 6)
+      assert(a < b)
+      assert(c > b)
+      assert(c == d)
     }
   }
 }
